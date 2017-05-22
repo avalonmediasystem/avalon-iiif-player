d2abf42bd57c19509d52792c387881548b60dd5e
Adam Arling <adam.arling@northwestern.edu>
2017-05-22 16:27:36 -0500

    Update svg package, standard js cleanup

--- app/avalon/media-player.js
+++ app/avalon/media-player.js
@@ -2,41 +2,39 @@ import HashHandler from './hash-handler'
 import $ from 'jquery'
 import '../../node_modules/mediaelement/src/css/mediaelementplayer.css'
 
-/** Class representing a MediaPlayer 
+/** Class representing a MediaPlayer
  * @class MediaPlayer
-*/
+ */
 export default class MediaPlayer {
   constructor (options) {
     /**
      * Create a MediaPlayer
      * @param {object} options - an object with the manifest and target
      */
-      this.manifest = options.manifest
-      this.target = document.getElementById(options.target)  
+    this.manifest = options.manifest
+    this.target = document.getElementById(options.target)
   }
   getLinks () {
-        /**
-         * this method sets the link on parent Ranges that don't have their own time, but inherit it from children in the tree
-         * 
-         * @method MediaPlayer#getLinks
-         */
-      $('.canvas-range').each((el) => {
-          console.log(el)
-        try {
-           
+    /**
+     * this method sets the link on parent Ranges that don't have their own time, but inherit it from children in the tree
+     *
+     * @method MediaPlayer#getLinks
+     */
+    $('.canvas-range').each((el) => {
+      console.log(el)
+      try {
         $(`.canvas-range:eq( ${el} )`).find('.canvas-url').attr('href', '' + this.getExtentForCanvas($('.canvas-range')[el], [], []))
       } catch (e) { console.log(e) }
-         })
-    
+    })
   }
 
   getSubtitles () {
-        /**
-         * this method gets the first subtitle track from the manifest. It will probaly need to more robust in the future
-         * 
-         * @method MediaPlayer#getSubtitles
-         * @return {string} subtitle - a URI that points to subtitles
-         */
+    /**
+     * this method gets the first subtitle track from the manifest. It will probaly need to more robust in the future
+     *
+     * @method MediaPlayer#getSubtitles
+     * @return {string} subtitle - a URI that points to subtitles
+     */
     var subtitle
     this.manifest.content[0].items.forEach((item) => {
       item.body.forEach((body) => {
@@ -50,11 +48,13 @@ export default class MediaPlayer {
 
   getQualityChoices () {
     var choices = []
-    if (this.manifest.content) {this.manifest.content[0].items.forEach((item) => {
-      item.body.forEach((body) => {
-        if (body.type === 'Choice') {
-          body.items.forEach((item) => {
-            choices.push(item)})
+    if (this.manifest.content) {
+      this.manifest.content[0].items.forEach((item) => {
+        item.body.forEach((body) => {
+          if (body.type === 'Choice') {
+            body.items.forEach((item) => {
+              choices.push(item)
+            })
           }
         })
       })
@@ -63,11 +63,11 @@ export default class MediaPlayer {
   }
 
   getVideoUri () {
-        /**
-         *  this method returns the URI with Medium quality from the manfest
-         * @method MediaPlayer#getVideoUri
-         * @return {string} uri - a URI for the medium quality video
-         */
+    /**
+     *  this method returns the URI with Medium quality from the manfest
+     * @method MediaPlayer#getVideoUri
+     * @return {string} uri - a URI for the medium quality video
+     */
     var uri
     this.manifest.content[0].items.forEach((item) => {
       item.body.forEach((body) => {
@@ -83,37 +83,37 @@ export default class MediaPlayer {
     return uri
   }
 
-      getMediaFragment (uri) {
-        /**
-         *
-         *  this takes a uri with a media fragment that looks like #=120,134 and returns an object with start/stop in seconds and the duration in milliseconds
-         * @method MediaPlayer#getMediaFragment
-         * 
-         * @return {object}
-         */
+  getMediaFragment (uri) {
+    /**
+     *
+     *  this takes a uri with a media fragment that looks like #=120,134 and returns an object with start/stop in seconds and the duration in milliseconds
+     * @method MediaPlayer#getMediaFragment
+     *
+     * @return {object}
+     */
 
-        if (uri !== undefined) {
-            const fragment = uri.split('#t=')[1]
-            if (fragment !== undefined) {
-                const splitFragment = fragment.split(',')
-                const duration = splitFragment[1] - splitFragment[0]
-                return { 'start': splitFragment[0],
-                         'stop': splitFragment[1] }
-            } else {
-                return undefined
-            }
-        } else {
-            return undefined
-        }
+    if (uri !== undefined) {
+      const fragment = uri.split('#t=')[1]
+      if (fragment !== undefined) {
+        const splitFragment = fragment.split(',')
+        const duration = splitFragment[1] - splitFragment[0]
+        return { 'start': splitFragment[0],
+          'stop': splitFragment[1] }
+      } else {
+        return undefined
+      }
+    } else {
+      return undefined
     }
+  }
 
   createStructure (manifest, list, canvasId) {
-        /**
-         * Recurses the manifest structure and creates an html tree
-         *  @method MediaPlayer#createStructure
-         * 
-         *  @return {string} list - a string version of the html tree
-         */
+    /**
+     * Recurses the manifest structure and creates an html tree
+     *  @method MediaPlayer#createStructure
+     *
+     *  @return {string} list - a string version of the html tree
+     */
     manifest.map((data, index) => {
       if (data.type === 'Range') {
         if (manifest[index].members[0].id !== undefined) {
@@ -137,18 +137,18 @@ export default class MediaPlayer {
     return list.join('')
   }
 
-    getExtentForCanvas (el, splits, newSplits) {
-        console.log(el)
-        /**
-         * This method takes a jQuery selector and calculates the extent of the parent based on the duration of the children
-         * 
-         * @method MediaPlayer#getExtentForCanvas
-         * 
-         * @param {string} el - a jQuery selector
-         * @param {array} splits - an empty array
-         * @param {array} newSplits - an empty array
-         * @return {string} - a mediafragment
-         **/
+  getExtentForCanvas (el, splits, newSplits) {
+    console.log(el)
+    /**
+     * This method takes a jQuery selector and calculates the extent of the parent based on the duration of the children
+     *
+     * @method MediaPlayer#getExtentForCanvas
+     *
+     * @param {string} el - a jQuery selector
+     * @param {array} splits - an empty array
+     * @param {array} newSplits - an empty array
+     * @return {string} - a mediafragment
+     **/
     $(el).children().find('a').each(function () {
       var splitHref = $(this).attr('href').split('#t=')
 
@@ -171,7 +171,7 @@ export default class MediaPlayer {
         canvasId = manifest[index].members[0].id
       }
       if (data.hasOwnProperty('members')) {
-          if (this.getMediaFragment(canvasId) !== undefined) {
+        if (this.getMediaFragment(canvasId) !== undefined) {
           list.push(`<ul><li><a class="media-structure-uri" data-media-fragment="${canvasId}">${data.label}</a></li>`)
           this.renderStructure(data.members, list, canvasId)
         } else {
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
 var path = require('path')
 module.exports = {
-    entry: path.resolve('./app/index.js'),
-    output: {
-        filename: './dist/bundle.js'
-    },
-    module: {
+  entry: path.resolve('./app/index.js'),
+  output: {
+    filename: './dist/bundle.js'
+  },
+  module: {
     rules: [
-        {
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env']
-                }
-            }
-        },
-        {
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-           
-        },         
-        {
-            test: /\.svg$/,
-            use: [ 'svg-inline-loader' ]
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+
+      },
+      {
+        test: /\.svg$/,
+        use: [ 'svg-url-loader' ]
+      }
     ]
-    }
+  }
 }
